5dee184e0076fadbc85d80ba24a51db0a89486d5
Bruno Luis Pereira <166344630+BrunoLuisPereira@users.noreply.github.com>
2026-04-01 20:21:58 -0300

    Update Remove-nth-node-from-end-of-list.py

--- algoritmos/Remove-nth-node-from-end-of-list.py
+++ algoritmos/Remove-nth-node-from-end-of-list.py
@@ -1,48 +1,21 @@
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+class Solution:
+    def removeNthFromEnd(self, head, n):
+        dummy = ListNode(0)
+        dummy.next = head
 
+        slow = dummy
+        fast = dummy
 
-def removeNthFromEnd(head, n):
-    dummy = ListNode(0)
-    dummy.next = head
+        # Avança o fast n+1 passos
+        for _ in range(n + 1):
+            fast = fast.next
 
-    slow = dummy
-    fast = dummy
+        # Move os dois ponteiros
+        while fast:
+            slow = slow.next
+            fast = fast.next
 
-    for _ in range(n + 1):
-        fast = fast.next
+        # Remove o nó
+        slow.next = slow.next.next
 
-    while fast:
-        slow = slow.next
-        fast = fast.next
-
-    slow.next = slow.next.next
-
-    return dummy.next
-
-
-def print_list(head):
-    current = head
-    while current:
-        print(current.val, end=" -> ")
-        current = current.next
-    print("None")
-
-
- 
-if __name__ == "__main__":
-    head = ListNode(1)
-    head.next = ListNode(2)
-    head.next.next = ListNode(3)
-    head.next.next.next = ListNode(4)
-    head.next.next.next.next = ListNode(5)
-
-    print("Lista original:")
-    print_list(head)
-
-    head = removeNthFromEnd(head, 2)
-
-    print("\nLista após remover:")
-    print_list(head)
+        return dummy.next
